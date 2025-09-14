1073f5206cbbfd0c792df6dfe8c215014b2e14f9
OscarGlad14 <oscarsimanullang141@gmail.com>
2025-09-14 15:32:32 +0700

    Mengimplementasi 4 fungsi show xml dan json di file views dan membuat path nya di file urls

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from main.views import show_main, show_product, create_product
+from main.views import show_main, show_product, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('show-product/<str:id>/', show_product, name='show_product'),
-    path('create-product/', create_product, name='create_product')
+    path('show-product/<str:product_id>/', show_product, name='show_product'),
+    path('create-product/', create_product, name='create_product'),
+    path('show-xml/', show_xml, name='show_xml'),
+    path('show-json/', show_json, name='show_json'),
+    path('show-xml-by-id/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('show-json-by-id/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
