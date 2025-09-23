e62757578904faabc0777c782c0a58fc81314987
OscarGlad14 <oscarsimanullang141@gmail.com>
2025-09-24 06:30:48 +0700

    Menyelesaikan tugas 4

--- main/models.py
+++ main/models.py
@@ -1,9 +1,12 @@
 import uuid
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
     CATEGORY_CHOICES = [
         ('sepatu', 'Sepatu'),
         ('pakaian', 'Pakaian'),
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_product, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, show_product, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -11,4 +11,7 @@ urlpatterns = [
     path('show-json/', show_json, name='show_json'),
     path('show-xml-by-id/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('show-json-by-id/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout')
 ]
